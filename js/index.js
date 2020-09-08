c0f5351f4891a0880ca7192b1cb5f4ec8a2e0770
Juan David <gottygarcia@gmail.com>
2020-09-07 23:19:14 -0500

    geez end

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ class game {
 		this.firstCardSelected
 		this.secondCardSelected
 		this.numberCardsSelected = 0
-		this.ContadorVictoria = 0
+		this.foundCouples = 0
 		this.moves = 0
-		this.containerMovimientos = document.getElementById('moves')
-		this.container = document.getElementById('game')
-		this.time = true
+		this.movesElement = document.getElementById('moves')
+		this.gameElement = document.getElementById('game')
 
 		//Timer
+		this.time = true
 		this.firstMove = false
 		this.hour = 0
 		this.minutes = 0
@@ -22,31 +22,38 @@ class game {
 	}
 
 	startGame() {
+		//change the "this" of the function
 		this.selectCard = this.selectCard.bind(this)
 
-		const LengthStatic = this.cardsNumbers.length
+		//Length = 10
+		const Length = this.cardsNumbers.length
 
-		for (let i = 0; i < LengthStatic; i++) {
+		//Add 10 more numbers to the number array to create pairs
+		for (let i = 0; i < Length; i++) {
 			this.cardsNumbers.push(this.cardsNumbers[i])
 		}
 
-		this.cards.length = this.cardsNumbers.length
-
+		//Shuffle the array randomly for the game
 		this.cardsNumbers = this.cardsNumbers.sort(function () {
 			return Math.random() - 0.5
 		})
 
+		//this.cards.length = 20 (number of cards)
+		this.cards.length = this.cardsNumbers.length
+
+		//Create the 20 cards and add their styles, events and corresponding html
 		for (let i = 0; i < this.cards.length; i++) {
 			this.cards[i] = document.createElement('div')
 			this.cards[i].classList.add('card')
 			this.cards[i].addEventListener('click', this.selectCard)
 			this.cards[
 				i
-			].innerHTML = `<div class="front vueltaFront" data-position="${i}"></div>
-										<div class="back vueltaBack" data-position="${i}" style="background-image: url('../img/cards/${this.cardsNumbers[i]}.png');"></div>`
-			this.container.appendChild(this.cards[i])
+			].innerHTML = `<div class="front cardFront" data-position="${i}"></div>
+										<div class="back cardBack" data-position="${i}" style="background-image: url('../img/cards/${this.cardsNumbers[i]}.png');"></div>`
+
+			//add the card to the container element
+			this.gameElement.appendChild(this.cards[i])
 		}
-		this.container.style.display = 'flex'
 	}
 
 	addEvents(n) {
@@ -61,40 +68,63 @@ class game {
 		if (this.time === true) {
 			switch (this.numberCardsSelected) {
 				case 0:
+					//Check if it is the first movement to initialize the timer
 					if (!this.firstMove) {
 						this.startTimer()
 					}
+					//initialize the timer
 					this.firstMove = true
+
+					//e.target.dataset.position = number assigned to the element in the startgame function
 					this.firstCardSelected = e.target.dataset.position
-					this.cards[this.firstCardSelected].classList.add('rotar')
+
+					//add the css class "rotateCard" to rotate the card
+					this.cards[this.firstCardSelected].classList.add('rotateCard')
+					//removes the events from the card so that it cannot be flipped while it is already flipped
 					this.removeEvents(this.firstCardSelected)
+
+					//increments the counter of selected cards
 					this.numberCardsSelected++
+
+					//increases movements and shows them
 					this.moves++
-					this.containerMovimientos.innerText = `Movimientos: ${this.moves}`
+					this.movesElement.innerText = `Moves: ${this.moves}`
 					break
 				case 1:
+					//increases movements and shows them
 					this.moves++
-					this.containerMovimientos.innerText = `Movimientos: ${this.moves}`
+					this.movesElement.innerText = `Moves: ${this.moves}`
+
+					//e.target.dataset.position = number assigned to the element in the startgame function
 					this.secondCardSelected = e.target.dataset.position
-					this.cards[this.secondCardSelected].classList.add('rotar')
+					//add the css class "rotateCard" to rotate the card
+					this.cards[this.secondCardSelected].classList.add('rotateCard')
+
+					//compares if the two selected cards have the same number
 					if (
 						this.cardsNumbers[this.firstCardSelected] ===
 						this.cardsNumbers[this.secondCardSelected]
 					) {
+						//remove the event from the second card as they match
 						this.removeEvents(this.secondCardSelected)
-						this.ContadorVictoria++
-						if (this.ContadorVictoria === this.cardsNumbers.length / 2) {
+						//increments the counter of pairs found
+						this.foundCouples++
+
+						//if the number of pairs found is equal to the number of cards (20/2) you win the game
+						if (this.foundCouples === this.cardsNumbers.length / 2) {
 							setTimeout(() => {
 								this.victory()
 							}, 1000)
 						}
 					} else {
 						this.time = false
+						//if they are not the same, remove the rotate class so that they remain as they were
 						setTimeout(() => {
-							this.cards[this.firstCardSelected].classList.remove('rotar')
-							this.cards[this.secondCardSelected].classList.remove('rotar')
+							this.cards[this.firstCardSelected].classList.remove('rotateCard')
+							this.cards[this.secondCardSelected].classList.remove('rotateCard')
 							this.time = true
 						}, 1000)
+						//add the event to the first card so that it can be flipped again
 						this.addEvents(this.firstCardSelected)
 					}
 					this.numberCardsSelected = 0
@@ -105,6 +135,7 @@ class game {
 
 	victory() {
 		this.PauseTime()
+		//"swal" is from a library imported into the game.html file
 		swal(
 			'You won the game!',
 			`Moves: ${this.moves} \n\n Time: ${this.timeText}`,
@@ -126,7 +157,7 @@ class game {
 	}
 
 	timer() {
-		if (this.stop == false) {
+		if (this.stop === false) {
 			this.decimals++
 			if (this.decimals > 9) {
 				this.decimals = 0
@@ -144,6 +175,7 @@ class game {
 			setTimeout('newGame.timer()', 100)
 		}
 	}
+
 	seeTimer() {
 		if (this.hour < 10) this.timeText = ''
 		else this.timeText = this.hour
@@ -153,6 +185,7 @@ class game {
 		this.timeText = this.timeText + this.seconds
 		document.getElementById('timeText').innerHTML = this.timeText
 	}
+
 	PauseTime() {
 		this.stop = true
 	}
